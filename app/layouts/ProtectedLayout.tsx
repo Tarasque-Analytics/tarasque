1ab7e3d5e0931602323bb5949dfcb004a90b80aa
Brian <nguyenbrian095@gmail.com>
2026-02-01 21:45:26 -0500

    feat: added header to Protected layout

--- app/layouts/ProtectedLayout.tsx
+++ app/layouts/ProtectedLayout.tsx
@@ -1,11 +1,50 @@
 // should wrap all auth-protected routes
 import { Outlet } from "react-router";
+import icon from "../assets/volarbear-icon.png";
+import name from "../assets/volarbear-name.png";
+import pfp from "../assets/profile-picture.png";
 
 export default function ProtectedLayout() {
   return (
-    <div>
-      <h2>Protected Layout</h2>
-      <Outlet />
+    <div className="p-6">
+      <div style={{ alignContent: "center"}} className="grid grid-cols-1 lg:grid-cols-3">
+        <div style={{ display: "flex", justifyContent: "flex-start", alignItems: "flex-start" }}>
+          <img
+            src={icon}
+            alt="Volarbear"
+            style={{
+              width: "75px",
+              height: "auto",
+              display: "block",
+            }}
+          />
+        </div>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "flex-start" }}>
+          <img
+            src={name}
+            alt="Volarbear"
+            style={{
+              width: "200px",
+              height: "auto",
+              display: "block",
+            }}
+          />
+        </div>
+        <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "flex-end" }}>
+          <img
+            src={pfp}
+            alt="Volarbear"
+            style={{
+              width: "75px",
+              height: "auto",
+              display: "block",
+            }}
+          />
+        </div>
+      </div>
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 }

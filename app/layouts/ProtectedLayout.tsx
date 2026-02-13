ff73e2cbae8547ce58234d2d4e6b5db3b2705839
Matthew <matthew.yu904@gmail.com>
2026-02-12 21:25:41 -0500

    resolved errors

--- app/layouts/ProtectedLayout.tsx
+++ app/layouts/ProtectedLayout.tsx
@@ -1,10 +1,10 @@
 // should wrap all auth-protected routes
 import { Outlet } from "react-router";
 import Navbar from "~/components/ui/navbar";
-import icon from "../assets/volarbear-icon.png";
-import name from "../assets/volarbear-name.png";
-import pfp from "../assets/profile-picture.png";
-import Search from "../components/ui/search";
+// import icon from "../assets/volarbear-icon.png";
+// import name from "../assets/volarbear-name.png";
+// import pfp from "../assets/profile-picture.png";
+// import Search from "../components/ui/search";
 
 export default function ProtectedLayout() {
   return (

1b1c86566c3223350fd61c63460c6cc02cb235da
Brian <nguyenbrian095@gmail.com>
2026-03-02 21:04:58 -0500

    refactor: removed unnecessary useStates from holdingView

--- app/components/dashboard/portfolio_components/holdingView.tsx
+++ app/components/dashboard/portfolio_components/holdingView.tsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router";
+import { useState } from "react";
 
 export interface HoldingProps {
-    ticker: string;
-    gain_loss: number;
+  ticker: string;
+  gain_loss: number;
 }
 
-// holding = {ticker: string, gain_loss: number} 
+// holding = {ticker: string, gain_loss: number}
 export default function HoldingView(holding: HoldingProps) {
-    const [symbol, setSymbol] = useState(holding.ticker);
+  const symbol = holding.ticker;
+  const percentChange = holding.gain_loss;
 
-    const [percentChange, setPercentChange] = useState(holding?.gain_loss || 0);
-
-    return (
-        <div className="ticker-gain-loss">
-            <div className="symbol">{symbol} -</div>
-            <div className={`percent-change ${percentChange >= 0 ? 'text-green-600' : 'text-red-600'}`}>
-                {percentChange.toFixed(2)}%
-            </div>
-        </div>
-    )
+  return (
+    <div className="ticker-gain-loss">
+      <div className="symbol">{symbol} -</div>
+      <div
+        className={`percent-change ${percentChange >= 0 ? "text-green-600" : "text-red-600"}`}
+      >
+        {percentChange.toFixed(2)}%
+      </div>
+    </div>
+  );
 }
